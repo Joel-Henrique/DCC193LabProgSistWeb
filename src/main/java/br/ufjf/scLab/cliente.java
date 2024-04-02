7cb28479331a85e5a04d1dab959b6ef1f099260a
Joel Henrique <104386844+Joel-Henrique@users.noreply.github.com>
2024-04-01 23:06:28 -0300

    class labsisweb

---
+++ src/main/java/br/ufjf/scLab/cliente.java
@@ -0,0 +1,11 @@
+package br.ufjf.scLab;
+
+public class cliente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String ruaEnd;
+    private int numeroEnd;
+    private String cep;
+
+}
---
+++ src/test/java/br/ufjf/scLab/ScLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.scLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
