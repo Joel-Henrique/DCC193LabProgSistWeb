cd3fc9059a812be8032db1c0100077e486e39f6e
Joel Henrique <104386844+Joel-Henrique@users.noreply.github.com>
2024-04-02 00:25:54 -0300

    Update Cliente.java

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -2,6 +2,7 @@ public class Cliente {
     private String nome;
     private String cpf;
     private String telefone;
+    private String email;
     private String ruaEnd;
     private int numeroEnd;
     private String cep;
